a13bdcfda303cbcfbf85d1943613f2a5a305e5c5
Bohdan <qwebeck5@gmail.com>
2024-02-15 01:30:08 +0100

    Added report and fixed figures

--- parallelized/build_on_device.sh
+++ parallelized/build_on_device.sh
@@ -1,7 +1,3 @@
-CC=nvcc
-GPU=vmgpu034
-OUT_NAME=gemm_cuda
-
 rsync -avz --exclude '.git' --exclude 'benchmark_data.csv' ../* forostib@vmgpu034.ensimag.fr:~/project
 ssh forostib@vmgpu034.ensimag.fr 'bash -l -c "cd project/parallelized && pwd && ls && make --always-make && ./mixture_models ../data.csv 3 10 2 means sigma 100 gpu_time.csv"'
 scp forostib@vmgpu034.ensimag.fr:~/project/parallelized/sigma forostib@vmgpu034.ensimag.fr:~/project/parallelized/means .
